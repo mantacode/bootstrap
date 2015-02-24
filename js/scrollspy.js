36257f0cd85b609eea46e27d7f85f00f2cee2b68
Eddie Canales <eddiecanales@gmail.com>
2015-02-24 16:34:06 -0500

    bump version to m3.1.1-m9

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: scrollspy.js v3.1.1-m7
+ * Bootstrap: scrollspy.js v3.1.1-m9
  * http://getbootstrap.com/javascript/#scrollspy
  * ========================================================================
  * Copyright 2011-2014 Twitter, Inc.
