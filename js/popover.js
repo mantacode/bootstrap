3c0e557d85ff379c7d165d142612485c0d48c1a9
Delightend <delightend@gmail.com>
2015-03-31 17:06:05 -0400

    trying to tag stuff

--- js/popover.js
+++ js/popover.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: popover.js v3.1.1-m13
+ * Bootstrap: popover.js v3.1.1-m9
  * http://getbootstrap.com/javascript/#popovers
  * ========================================================================
  * Copyright 2011-2014 Twitter, Inc.
--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: scrollspy.js v3.1.1-m13
+ * Bootstrap: scrollspy.js v3.1.1-m9
  * http://getbootstrap.com/javascript/#scrollspy
  * ========================================================================
  * Copyright 2011-2014 Twitter, Inc.
