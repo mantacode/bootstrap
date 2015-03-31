ea236c2d6e003a75716a814893580c8cde2f3dd6
Delightend <delightend@gmail.com>
2015-03-31 16:49:39 -0400

    trying to correctly update version

--- js/affix.js
+++ js/affix.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: affix.js v3.1.1-m9
+ * Bootstrap: affix.js v3.1.1-m13
  * http://getbootstrap.com/javascript/#affix
  * ========================================================================
  * Copyright 2011-2014 Twitter, Inc.
--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: scrollspy.js v3.1.1-m9
+ * Bootstrap: scrollspy.js v3.1.1-m13
  * http://getbootstrap.com/javascript/#scrollspy
  * ========================================================================
  * Copyright 2011-2014 Twitter, Inc.
