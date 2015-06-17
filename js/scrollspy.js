c92ed2a73514289f9aa4f4f48b1e1866ee0e209b
Andrew Nichols <anichols@manta.com>
2015-06-17 08:58:43 -0400

    Version 3.1.1-1.0.2

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: scrollspy.js v3.1.1-1.0.1
+ * Bootstrap: scrollspy.js v3.1.1-1.0.2
  * http://getbootstrap.com/javascript/#scrollspy
  * ========================================================================
  * Copyright 2011-2014 Twitter, Inc.
