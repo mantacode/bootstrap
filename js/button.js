248ce0053dd86dad5c9ffa3653786093180dae73
Delightend <delightend@gmail.com>
2015-10-01 10:04:53 -0400

    bumped version

--- js/button.js
+++ js/button.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: button.js v3.1.1-1.0.4
+ * Bootstrap: button.js v3.1.1-1.0.5
  * http://getbootstrap.com/javascript/#buttons
  * ========================================================================
  * Copyright 2011-2014 Twitter, Inc.
--- js/tooltip.js
+++ js/tooltip.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: tooltip.js v3.1.1-1.0.4
+ * Bootstrap: tooltip.js v3.1.1-1.0.5
  * http://getbootstrap.com/javascript/#tooltip
  * Inspired by the original jQuery.tipsy by Jason Frame
  * ========================================================================
