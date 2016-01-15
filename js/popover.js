ed534a515841c83ac552214610ddbd3ba7ea2739
Andrew Nichols <anichols@manta.com>
2016-01-15 09:27:48 -0500

    Version 3.1.1-1.0.6

--- js/popover.js
+++ js/popover.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: popover.js v3.1.1-1.0.5
+ * Bootstrap: popover.js v3.1.1-1.0.6
  * http://getbootstrap.com/javascript/#popovers
  * ========================================================================
  * Copyright 2011-2014 Twitter, Inc.
--- js/transition.js
+++ js/transition.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Bootstrap: transition.js v3.1.1-1.0.5
+ * Bootstrap: transition.js v3.1.1-1.0.6
  * http://getbootstrap.com/javascript/#transitions
  * ========================================================================
  * Copyright 2011-2014 Twitter, Inc.
